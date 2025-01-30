a9de463a6f4bb053516e5500a48996e66e3accae
Denis Stenyushkin <denisstenyushkin@yandex.ru>
2025-01-30 16:39:15 +0000

    Basic db impl

---
+++ hw11/database.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <sqlite3.h>
+
+
+class Database {
+public:
+    using JoinRow = std::vector<std::tuple<int, std::string, std::string>>;
+
+    Database() {
+        char* zErrMsg = nullptr;
+        int rc;
+
+        rc = sqlite3_open("", &db_);
+        if (rc) {
+            throw std::runtime_error("Unable to open database");
+        }
+
+        const char* tableA_sql = "CREATE TABLE A(" \
+                                 "ID INT PRIMARY KEY  NOT NULL," \
+                                 "NAME           TEXT NOT NULL);";
+        const char* tableB_sql = "CREATE TABLE B(" \
+                                 "ID INT PRIMARY KEY  NOT NULL," \
+                                 "NAME           TEXT NOT NULL);";
+        
+        rc = sqlite3_exec(db_, tableA_sql, nullptr, 0, &zErrMsg);
+        if (rc) {
+            throw std::runtime_error("Unable to create table A");
+        }
+
+        rc = sqlite3_exec(db_, tableB_sql, nullptr, 0, &zErrMsg);
+        if (rc) {
+            throw std::runtime_error("Unable to create table B");
+        }
+    }
+
+    void insert(const std::string& table, int id, const std::string& name) {
+        std::stringstream sql;
+        int rc;
+        char* zErrMsg = nullptr;
+
+        sql << "INSERT INTO " << table << " (ID, NAME) ";
+        sql << "VALUES (" << id << ", '" << name << "');";
+        std::cout << "SQL: " << sql.str() << std::endl;
+
+        rc = sqlite3_exec(db_, sql.str().c_str(), nullptr, 0, &zErrMsg);
+        if (rc) {
+            throw std::runtime_error("Unable to execute " + sql.str());
+        }
+    }
+
+    void truncate(const std::string& table) {
+        std::stringstream sql;
+        int rc;
+        char* zErrMsg = nullptr;
+
+        sql << "DELETE FROM " << table << ";";
+        std::cout << "SQL: " << sql.str() << std::endl;
+
+        rc = sqlite3_exec(db_, sql.str().c_str(), nullptr, 0, &zErrMsg);
+        if (rc) {
+            throw std::runtime_error("Unable to execute " + sql.str());
+        }
+    }
+
+    JoinRow intersection() {
+        return exec_join_sql("SELECT A.id, A.name, B.name " \
+                             "FROM A INNER JOIN B " \
+                             "ON A.id = B.id " \
+                             "ORDER BY A.ID ASC;");
+    }
+
+    JoinRow symmetric_difference() {
+        std::stringstream sql;
+        sql << "SELECT A.ID, A.name, B.name "
+               "FROM A LEFT JOIN B "
+               "ON A.id = B.id "
+               "UNION ALL "
+               "SELECT B.id, A.name, B.name "
+               "FROM B LEFT JOIN A "
+               "ON B.id = A.id "
+               "EXCEPT "
+               "SELECT A.id, A.name, B.name "
+               "FROM A INNER JOIN B "
+               "ON A.id = B.id;";
+        return exec_join_sql(sql.str().c_str());
+    }
+
+    ~Database() {
+        sqlite3_close(db_);
+    }
+
+private:
+     JoinRow exec_join_sql(const char* sql) {
+        sqlite3_stmt* stmt = nullptr;
+        int rc;
+        JoinRow result;
+
+        rc = sqlite3_prepare_v2(db_, sql, -1, &stmt, nullptr);
+        if (rc) {
+            throw std::runtime_error("Unable to execute " + std::string(sql));
+        }
+
+        rc = sqlite3_step(stmt);
+        while (rc != SQLITE_DONE && rc != SQLITE_OK) {
+            int id = sqlite3_column_int(stmt, 0);
+            const unsigned char* name1 =  sqlite3_column_text(stmt, 1);
+            const unsigned char* name2 =  sqlite3_column_text(stmt, 2);
+
+            std::string name1_s = name1 != nullptr ? std::string((const char*)name1) : "";
+            std::string name2_s = name2 != nullptr ? std::string((const char*)name2) : "";
+
+            result.emplace_back(id, name1_s, name2_s);
+            rc = sqlite3_step(stmt);
+        }
+
+        rc = sqlite3_finalize(stmt);
+
+        return result;
+     }
+
+    sqlite3* db_;
+};
--- hw11/join_server.cpp
+++ hw11/join_server.cpp
@@ -1,12 +1,27 @@
 #include <cstdlib>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "boost/asio.hpp"
 
+#include "database.hpp"
 #include "message.hpp"
 
+
+std::vector<std::string> split(const std::string& s, const char delimiter) {
+    std::stringstream sstream{s};
+    std::string segment;
+    std::vector<std::string> seglist;
+
+    while(std::getline(sstream, segment, delimiter))
+    {
+        seglist.push_back(segment);
+    }
+
+    return seglist;
+}
 
 class Session : public std::enable_shared_from_this<Session> {
 public:
@@ -37,16 +52,66 @@ private:
                                     if (!ec) {
                                         std::cout << "Received: " << read_msg_.body() << std::endl;
 
-                                        Message msg;
-                                        const char* line = "Ping back";
-                                        msg.body_length(std::strlen(line));
-                                        std::memcpy(msg.body(), line, msg.body_length());
-                                        msg.encode_header();
-                                        do_write(msg);
+                                        try {
+                                            std::vector<std::string> tokens = split(read_msg_.body(), ' ');
+                                            if (tokens.empty()) {
+                                                return;
+                                            }
+
+                                            // TODO: Error handling!!!
+                                            if (tokens[0] == "INSERT") {
+                                                db_.insert(tokens[1], std::stoi(tokens[2]), tokens[3]);
+                                                send_response("OK");
+                                            }
+                                            else if (tokens[0] == "TRUNCATE") {
+                                                db_.truncate(tokens[1]);
+                                                send_response("OK");
+                                            }
+                                            else if (tokens[0] == "INTERSECTION") {
+                                                auto result = db_.intersection();
+                                                
+                                                std::stringstream response;
+                                                for (auto& [id, name1, name2]: result) {
+                                                    response << id << "," << name1 << "," << name2 << std::endl;
+                                                }
+                                                response << "OK";
+
+                                                send_response(response.str().c_str());
+                                            }
+                                            else if (tokens[0] == "SYMMETRIC_DIFFERENCE") {
+                                                auto result = db_.symmetric_difference();
+                                                
+                                                std::stringstream response;
+                                                for (auto& [id, name1, name2]: result) {
+                                                    response << id << "," << name1 << "," << name2 << std::endl;
+                                                }
+                                                response << "OK";
+
+                                                send_response(response.str().c_str());
+                                            }
+                                            else {
+                                                std::stringstream response;
+                                                response << "Unknown operation: " << tokens[0];
+                                                send_response(response.str().c_str());
+                                            }
+                                        } 
+                                        catch (const std::exception& ex) {
+                                            std::stringstream response;
+                                            response << "Unexpected error: " << ex.what();
+                                            send_response(response.str().c_str());
+                                        }
 
                                         do_read_header();
                                     }
                                 });
+    }
+
+    void send_response(const char* line) {
+        Message msg;
+        msg.body_length(std::strlen(line));
+        std::memcpy(msg.body(), line, msg.body_length());
+        msg.encode_header();
+        do_write(msg);
     }
 
     void do_write(Message send_msg) {
@@ -63,6 +128,7 @@ private:
     boost::asio::ip::tcp::socket socket_;
     char data_[Message::header_length + Message::max_body_length];
     Message read_msg_;
+    Database db_;
 };
 
 class Server {
